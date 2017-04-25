d4e7cc53c70cd7cfa7daaf5d267f35c686cb6138
momikaMichal <michaldaisey@gmail.com>
2017-04-25 10:26:16 +0300

    Initial Project

---
+++ src/HomeWork2/DecisionTree.java
@@ -0,0 +1,69 @@
+package HomeWork2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import weka.classifiers.Classifier;
+import weka.core.Capabilities;
+import weka.core.Instance;
+import weka.core.InstanceComparator;
+import weka.core.Instances;
+
+class BasicRule {
+    int attributeIndex;
+	int attributeValue;
+}
+
+class Rule {
+   	List<BasicRule> basicRule;
+   	double returnValue;
+}
+
+class Node {
+	Node[] children;
+	Node parent;
+	int attributeIndex;
+	double returnValue;
+   	Rule nodeRule = new Rule();
+
+}
+
+public class DecisionTree implements Classifier {
+	private Node rootNode;
+	public enum PruningMode {None, Chi, Rule};
+	private PruningMode m_pruningMode;
+   	Instances validationSet;
+   	private List<Rule> rules = new ArrayList<Rule>();
+
+	@Override
+	public void buildClassifier(Instances arg0) throws Exception {
+
+	}
+
+	public void setPruningMode(PruningMode pruningMode) {
+		m_pruningMode = pruningMode;
+	}
+	
+	public void setValidation(Instances validation) {
+		validationSet = validation;
+	}
+    
+    @Override
+	public double classifyInstance(Instance instance) {
+		//TODO: implement this method
+        return 0;
+	}
+    
+    @Override
+	public double[] distributionForInstance(Instance arg0) throws Exception {
+		// Don't change
+		return null;
+	}
+
+	@Override
+	public Capabilities getCapabilities() {
+		// Don't change
+		return null;
+	}
+
+}
