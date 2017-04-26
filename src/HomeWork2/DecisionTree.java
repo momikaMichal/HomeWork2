2cb7138a0eea3cb16a6a431cbc04b00a7c0955d1
momikaMichal <michaldaisey@gmail.com>
2017-04-26 19:43:12 +0300

    Implementation of buildTree

--- src/HomeWork2/DecisionTree.java
+++ src/HomeWork2/DecisionTree.java
@@ -1,69 +1,117 @@
 package HomeWork2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.Queue;
 
 import weka.classifiers.Classifier;
-import weka.core.Capabilities;
-import weka.core.Instance;
-import weka.core.InstanceComparator;
-import weka.core.Instances;
+import weka.core.*;
 
 class BasicRule {
     int attributeIndex;
-	int attributeValue;
+    int attributeValue;
 }
 
 class Rule {
-   	List<BasicRule> basicRule;
-   	double returnValue;
+    List<BasicRule> basicRule;
+    double returnValue;
 }
 
 class Node {
-	Node[] children;
-	Node parent;
-	int attributeIndex;
-	double returnValue;
-   	Rule nodeRule = new Rule();
+    Node[] children;
+    Node parent;
+    int attributeIndex;
+    double returnValue;
+    Rule nodeRule = new Rule();
+    Instances instances;
 
+    public boolean isPerfectlyClassified() {
+        //TODO: Implement isPerfectlyClassified
+        return true;
+    }
 }
 
 public class DecisionTree implements Classifier {
-	private Node rootNode;
-	public enum PruningMode {None, Chi, Rule};
-	private PruningMode m_pruningMode;
-   	Instances validationSet;
-   	private List<Rule> rules = new ArrayList<Rule>();
+    private Node rootNode;
 
-	@Override
-	public void buildClassifier(Instances arg0) throws Exception {
+    public enum PruningMode {None, Chi, Rule}
 
-	}
+    private PruningMode m_pruningMode;
+    Instances validationSet;
+    private List<Rule> rules = new ArrayList<Rule>();
+    private Queue<Node> m_nodesQueue;
 
-	public void setPruningMode(PruningMode pruningMode) {
-		m_pruningMode = pruningMode;
-	}
-	
-	public void setValidation(Instances validation) {
-		validationSet = validation;
-	}
-    
     @Override
-	public double classifyInstance(Instance instance) {
-		//TODO: implement this method
+    public void buildClassifier(Instances arg0) throws Exception {
+
+        m_nodesQueue = new LinkedList<>();
+        m_nodesQueue.add(rootNode);
+
+        // Build the tree
+        buildTree(arg0);
+    }
+
+    private void buildTree(Instances data) {
+        Node currentNode = m_nodesQueue.remove();
+
+        while (!m_nodesQueue.isEmpty()) {
+            if (!currentNode.isPerfectlyClassified()) {
+                Attribute bestAttribute = findBestAttribute(currentNode); // color
+                Enumeration<Object> attributeValues = bestAttribute.enumerateValues(); // green yellow red
+                currentNode.children = new Node[bestAttribute.numValues()];
+
+                int i = 0;
+                while (attributeValues.hasMoreElements()) {
+                    Object attributeValue = attributeValues.nextElement();
+                    for (Instance instance : currentNode.instances) {
+                        String bestAttributeValueOfCurrentInstance = instance.stringValue(bestAttribute);
+                        Node currentChild;
+                        if (bestAttributeValueOfCurrentInstance.equals(attributeValue)) {
+                            currentChild = currentNode.children[i];
+                            currentChild.instances.add(instance);
+                        }
+                    }
+                    i++;
+                }
+
+                // Add the children to the queue
+                for (Node child : currentNode.children) {
+                    m_nodesQueue.add(child);
+                }
+            }
+
+            // Move to the next node
+            currentNode = m_nodesQueue.remove();
+        }
+    }
+
+    private Attribute findBestAttribute(Node node) {
+        //TODO: Implement findBestAttribute
+        return null;
+    }
+
+    public void setPruningMode(PruningMode pruningMode) {
+        m_pruningMode = pruningMode;
+    }
+
+    public void setValidation(Instances validation) {
+        validationSet = validation;
+    }
+
+    @Override
+    public double classifyInstance(Instance instance) {
+        //TODO: implement classifyInstance
         return 0;
-	}
-    
+    }
+
     @Override
-	public double[] distributionForInstance(Instance arg0) throws Exception {
-		// Don't change
-		return null;
-	}
+    public double[] distributionForInstance(Instance arg0) throws Exception {
+        // Don't change
+        return null;
+    }
 
-	@Override
-	public Capabilities getCapabilities() {
-		// Don't change
-		return null;
-	}
-
+    @Override
+    public Capabilities getCapabilities() {
+        // Don't change
+        return null;
+    }
 }
